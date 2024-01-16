23959aaddfe49cd1ce9407484d214c512440bc8a
Sams137 <92424088+Sams137@users.noreply.github.com>
2024-01-16 15:42:18 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const myHeading = document.querySelector("h1");
 const myImage = document.querySelector("img");
 let myButton = document.querySelector("button");
+var id = null;
 
 myHeading.textContent = "Sam's Page";
 
@@ -29,3 +30,19 @@ if (!localStorage.getItem("name")) {
 myButton.onclick = () => {
   setUserName();
 };
+
+function myMove() {
+  var elem = document.getElementById("myAnimation");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    }
+  }
+}
