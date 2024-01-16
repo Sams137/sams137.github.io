d35a97a35146130c03807c14de2d635544cc7bd4
Sams137 <92424088+Sams137@users.noreply.github.com>
2024-01-16 15:07:46 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,6 @@ myImage.onclick = () => {
 };
 
 let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
 
 function setUserName() {
   const myName = prompt("Please enter your name.");
