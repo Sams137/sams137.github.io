da637e9396a8010bd543491465440160bc4b3cc9
Sams137 <92424088+Sams137@users.noreply.github.com>
2024-01-16 14:53:50 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Sam's Page";
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/cup.jpg") {
+    myImage.setAttribute("src", "images/cup_sat.jpg");
+  } else {
+    myImage.setAttribute("src", "images/cup.jpg");
+  }
+};
