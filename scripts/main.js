d73fb92c8e13b227e84316f85e93d0b37b444445
Sams137 <92424088+Sams137@users.noreply.github.com>
2024-01-26 10:28:37 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -71,6 +71,19 @@ function myMove() {
   }
 }
 
+function display_c(){
+   var refresh=1000; // Refresh rate in milli seconds
+   mytime=setTimeout('display_ct()',refresh)
+}
+
+function display_ct() {
+  var CDate = new Date()
+  var NewDate=CDate.toDateString(); 
+  NewDate = NewDate + " - " + CDate.toLocaleTimeString();
+  document.getElementById('ct').innerHTML = NewDate;
+  display_c();
+}
+
 /***********
  * Actions *
  ***********/
